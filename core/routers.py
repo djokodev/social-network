6540356accbba41d114a4e33ae8a7de1040f19ae
M@teo22 <djokodev75@gmail.com>
2024-12-11 02:03:31 +0100

    feat: add userViewsets and router

---
+++ core/routers.py
@@ -0,0 +1,10 @@
+from rest_framework import routers
+from core.user.viewsets import UserViewset
+
+
+router = routers.SimpleRouter()
+router.register(r'user', UserViewset, basename='user')
+
+urlpatterns = [
+    *router.urls,
+]
